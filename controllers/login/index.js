64d66f8eb3de2a959bb5c264c7d5a839579b385f
split <voicesplit@daum.net>
2021-07-29 13:41:23 +0900

    passport 미들웨어로 분리

--- controllers/login/index.js
+++ controllers/login/index.js
@@ -1,39 +1,7 @@
 const { Router } = require('express');
 const router = Router();
 const ctrl = require('./login.ctrl');
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const passwordHash = require('../../helpers/passwordHash');
-const models = require('../../models');
-
-passport.use(new LocalStrategy({
-  usernameField : 'userid',
-  passwordField : 'password',
-  passReqToCallback : true
-}, async (req, userid, password, done)=>{
-  const user = await models.User.findOne({
-    where : {
-      userid : req.body.userid, 
-      password : passwordHash(password)
-    }
-  })
-  if(!user){
-    return done(null, false, )
-  } else {
-    return done(null, user.dataValues) //user.dataValues가 serializeUser의 user파라미터에 전달
-  }
-}
-));
-
-passport.serializeUser((user, done)=>{
-  console.log('serializeUser work');
-  done( null, user ); //최종적으로 세션에 담기는 내용
-})
-
-passport.deserializeUser((user, done)=>{
-  console.log('deserializeUser work');
-  done( null, user );
-})
+const passport = require('../../middleware/passport-local');
 
 router.get('/', ctrl.get_login_main);
 
---
+++ middleware/passport-local.js
@@ -0,0 +1,35 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const passwordHash = require('../helpers/passwordHash');
+const models = require('../models');
+
+passport.use(new LocalStrategy({
+  usernameField : 'userid',
+  passwordField : 'password',
+  passReqToCallback : true
+}, async (req, userid, password, done)=>{
+  const user = await models.User.findOne({
+    where : {
+      userid : req.body.userid, 
+      password : passwordHash(password)
+    }
+  })
+  if(!user){
+    return done(null, false, )
+  } else {
+    return done(null, user.dataValues) //user.dataValues가 serializeUser의 user파라미터에 전달
+  }
+}
+));
+
+passport.serializeUser((user, done)=>{
+  console.log('serializeUser work');
+  done( null, user ); //최종적으로 세션에 담기는 내용
+})
+
+passport.deserializeUser((user, done)=>{
+  console.log('deserializeUser work');
+  done( null, user );
+})
+
+module.exports = passport;
