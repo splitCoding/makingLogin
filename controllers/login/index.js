30e00871b77cb7f7ffc2ac8ca8382a8a3d52fad3
split <voicesplit@daum.net>
2021-07-29 13:10:20 +0900

    passport 정책작성

--- controllers/login/index.js
+++ controllers/login/index.js
@@ -1,6 +1,28 @@
 const { Router } = require('express');
 const router = Router();
 const ctrl = require('./login.ctrl');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const passwordHash = require('../../helpers/passwordHash');
+const models = require('../../models');
+
+passport.use(new LocalStrategy({
+  usernameField : 'userid',
+  passwordField : 'password',
+  passReqToCallback : true
+}, async (req, userid, password, done)=>{
+  const user = await models.User.findOne({
+    where : {
+      userid, password : passwordHash(password)
+    }
+  })
+  if(!user){
+    return done(null, false)
+  } else {
+    return done(null, user.dataValues)
+  }
+}
+));
 
 router.get('/', ctrl.get_login_main);
 
