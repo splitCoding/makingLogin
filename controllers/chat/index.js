3f1b9158badb530d3639c55fea4b0ed68994473e
split <voicesplit@daum.net>
2021-07-31 16:37:47 +0900

    로그인시에만 채팅가능

--- controllers/chat/index.js
+++ controllers/chat/index.js
@@ -3,7 +3,11 @@ const router = Router();
 const csrfProtection = require('../../middleware/csrf');
 
 router.get('/', csrfProtection, (req, res)=>{
-  res.render('chat/chat.html',{ message : req.csrfToken() });
+  if(req.isAuthenticated()){
+    res.render('chat/chat.html');
+  } else {
+    res.send('<script>alert("로그인이 필요합니다"); \location.href="/login"</script>');
+  }
 })
 
 module.exports = router;
